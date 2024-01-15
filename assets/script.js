04de2ea841069b4b05c570fd845a3223f4fb49d8
Nourdine Belkhadir <140583027+nour-bel-nour@users.noreply.github.com>
2024-01-15 22:50:33 +0100

    Ajout bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -18,9 +18,11 @@ const slides = [
   },
 ];
   
-  //... Ajout des Event Listeners sur les flèches 
-  let flecheGauche = document.querySelector("#banner .arrow_left")
-  let flecheDroite = document.querySelector("#banner .arrow_right")
+
+//.......Ajout des Event Listeners sur les flèches....... 
+  
+  const flecheGauche = document.querySelector("#banner .arrow_left")
+  const flecheDroite = document.querySelector("#banner .arrow_right")
 
   flecheGauche.addEventListener('click', () => {
     console.log('Gauche')
@@ -29,3 +31,26 @@ const slides = [
   flecheDroite.addEventListener('click', () => {
     console.log('Droite')
   })
+
+
+//.......Ajout bullet points au slider.......
+
+  // Boucle pour créer un point pour chaque image du caroussel
+  slides.forEach((slide, index) => {
+  // Créer une div et sa class dot
+  const point = document.createElement('div');
+        point.classList.add('dot');
+  // Ajout la classe dot_selected pour le premier point 
+  if (index === 0) { point.classList.add('dot_selected');}
+ // Ajout du point au conteneur des points du carrousel
+  const pointsConteneur = document.querySelector('#banner .dots');
+        pointsConteneur.appendChild(point);
+  });
+
+
+  
+
+  
+        
+
+
