c47b6d6adec2ef8208455123fca3d36e024a4dcd
Nourdine Belkhadir <140583027+nour-bel-nour@users.noreply.github.com>
2024-01-19 22:45:56 +0100

    Modification du slide au clic sur bouton (ajout images,point,texte)

--- assets/script.js
+++ assets/script.js
@@ -16,41 +16,52 @@ const slides = [
     image: 'slide4.png',
     tagLine: 'Autocollants <span>avec découpe laser sur mesure</span>',
   },
-];
+]
   
 
-//.......Ajout des Event Listeners sur les flèches....... 
+///...2....Ajout des Event Listeners sur les flèches....... 
   
   const flecheGauche = document.querySelector("#banner .arrow_left")
   const flecheDroite = document.querySelector("#banner .arrow_right")
 
-  flecheGauche.addEventListener('click', () => {
-    console.log('Gauche')
-  })
-
-  flecheDroite.addEventListener('click', () => {
-    console.log('Droite')
-  })
+        flecheGauche.addEventListener('click', () => {ChangeCarrousel('prev')})
+        flecheDroite.addEventListener('click', () => {ChangeCarrousel('next')})
 
 
-//.......Ajout bullet points au slider.......
-
+///...3....Ajout bullet points au slider.......c 
+   
   // Boucle pour créer un point pour chaque image du caroussel
   slides.forEach((slide, index) => {
   // Créer une div et sa class dot
-  const point = document.createElement('div');
-        point.classList.add('dot');
+  const point = document.createElement('div')
+        point.classList.add('dot')
   // Ajout la classe dot_selected pour le premier point 
-  if (index === 0) { point.classList.add('dot_selected');}
- // Ajout du point au conteneur des points du carrousel
-  const pointsConteneur = document.querySelector('#banner .dots');
-        pointsConteneur.appendChild(point);
-  });
+  if (index === 0) {point.classList.add('dot_selected')}  
+  // Ajout du point au conteneur des points du carrousel
+  const pointsConteneur = document.querySelector('#banner .dots')
+        pointsConteneur.appendChild(point)
+  })
 
 
+///...4....Modification du slide au clic sur bouton (ajout images,point,texte).......
   
+  const points  = document.querySelectorAll('#banner .dots .dot')
+  const images  = document.querySelectorAll('#banner .banner-img')
+  const textes  = document.querySelector   ('#banner p')
 
+  function miseàJourCarrousel(i)           {
+    
+    // changement point actif au suivant
+    points.forEach((point, e) => {
+    if (e === i) {point.classList.add   ('dot_selected')} 
+    else         {point.classList.remove('dot_selected')}    })  
+    
+    // changement image
+    images.forEach((image)    => {
+    image.src = `assets/images/slideshow/${slides[i].image}`
+    image.alt = `         Banner Print-it${slides[i].image}` })
   
-        
+    // changement texte
+    textes.innerHTML = slides[i].tagLine  }
 
 
